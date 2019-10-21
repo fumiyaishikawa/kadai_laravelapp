154c29b9a61c2e2388ba95dfba093b903350404c
fumiya ishikawa <programer.ishikawa@gmail.com>
2019-10-21 14:27:29 +0900

    親レイアウトを作成しました

--- kadaiapp/resources/views/login.blade.php
+++ kadaiapp/resources/views/login.blade.php
@@ -1,35 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>ToDo list</title>
-</head>
-<body>
-    <header>
-        <h2>Laravel</h2>
-        <p><a href="/login">Login</a></p>
-        <p><a href="/register">Register</a></p>
-    </header>
+@extends('layouts.parent')
 
-    <div class="todo-block">
-        <h2>Login</h2>
-        <form action="/mytask" method="post">
-            {{csrf_field() }}
-            <p>
-                <label for="mail">E-Mail ddress</label>
-                <input type="text" name="mail" id="mail">
-            </p>
-            <p>
-                <label for="pass">Password</label>
-                <input type="text" name="pass" id="pass">
-            </p>
-            <p>
-                <input type="checkbox" name="check" id="check">
-                <label for="check">Remember Me</label>
-            </p>
-            <button type="submit">Login</button>
-            <p><a href="/register">Forget Your Password?</a></p>
-        </form>
-    </div>
-</body>
-</html>
+{{-- TitleとH2 --}}
+@section('title', 'Login')
+
+{{-- 個別ページの内容 --}}
+@section('content')
+<form action="/mytask" method="post">
+    {{csrf_field() }}
+    <p>
+        <label for="mail">E-Mail ddress</label>
+        <input type="text" name="mail" id="mail">
+    </p>
+    <p>
+        <label for="pass">Password</label>
+        <input type="text" name="pass" id="pass">
+    </p>
+    <p>
+        <input type="checkbox" name="check" id="check">
+        <label for="check">Remember Me</label>
+    </p>
+    <button type="submit">Login</button>
+    <p><a href="/register">Forget Your Password?</a></p>
+</form>
+@endsection
--- kadaiapp/resources/views/register.blade.php
+++ kadaiapp/resources/views/register.blade.php
@@ -1,38 +1,28 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>ToDo list</title>
-</head>
-<body>
-    <header>
-        <h2>Laravel</h2>
-        <p><a href="/login">Login</a></p>
-        <p><a href="/register">Register</a></p>
-    </header>
+@extends('layouts.parent')
 
-    <div class="todo-block">
-        <h2>Register</h2>
-        <form action="/register.check" method="post">
-            {{csrf_field() }}
-            <p>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name">
-            </p>
-            <p>
-                <label for="mail">E-Mail ddress</label>
-                <input type="text" name="mail" id="mail">
-            </p>
-            <p>
-                <label for="pass">Password</label>
-                <input type="text" name="pass" id="pass">
-            </p>
-            <p>
-                <label for="conf_pass">Confirm Password</label>
-                <input type="text" name="conf_pass" id="conf_pass">
-            </p>
-            <button type="submit">Register</button>
-        </form>
-    </div>
-</body>
-</html>
+{{-- TitleとH2 --}}
+@section('title', 'Register')
+
+{{-- 個別ページの内容 --}}
+@section('content')
+<form action="/register.check" method="post">
+    {{csrf_field() }}
+    <p>
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="mail">E-Mail ddress</label>
+        <input type="text" name="mail" id="mail">
+    </p>
+    <p>
+        <label for="pass">Password</label>
+        <input type="text" name="pass" id="pass">
+    </p>
+    <p>
+        <label for="conf_pass">Confirm Password</label>
+        <input type="text" name="conf_pass" id="conf_pass">
+    </p>
+    <button type="submit">Register</button>
+</form>
+@endsection
